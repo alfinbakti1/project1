68fa94dce3d7d4249ab831834edaa87ba092c795
Alfin_bakti <alfinmoklet@gmail.com>
2017-03-20 14:03:44 +0700

    project first git

--- app/src/main/java/com/example/alfin_bakti/myapplication/MainActivity.java
+++ app/src/main/java/com/example/alfin_bakti/myapplication/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     RadioGroup radioGroup;
     int h1, bt, ti;
     String temp;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
